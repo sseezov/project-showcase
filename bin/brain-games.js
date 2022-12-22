028ea7e3bf6e33ce69b5a452e1a9149be12657ae
sseezov <sseezov@mail.ru>
2022-12-22 13:30:08 +0700

    third step done

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,4 @@
 #!/usr/bin/env node
-console.log('Welcome');
+import {name} from '../src/cli.js'
+
+console.log(`Hello, ${name}!`);
---
+++ src/cli.js
@@ -0,0 +1,4 @@
+import readlineSync from 'readline-sync';
+
+export const name = readlineSync.question(`Welcome to the Brain Games!
+May I have your name? `);
